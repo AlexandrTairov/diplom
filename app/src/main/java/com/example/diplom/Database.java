584f864b997ae5dfc81ae04d9b1a42fbd157aa8f
Александр <sanek.tai0@gmail.com>
2021-05-21 11:35:21 +0400

    start commit

---
+++ app/src/main/java/com/example/diplom/Database.java
@@ -0,0 +1,21 @@
+package com.example.diplom;
+
+public class Database {
+    public static final String TABLE_NAME_KEYPHRASE = " ";
+
+    public static String id;
+
+    public static String value;
+
+    public static String phrase_1;
+
+    public static String phrase_2;
+
+    public static String phrase_3;
+
+    public static String phrase_4;
+
+    public static String TTSPhrase_1;
+
+    public static String TTSPhrase_2;
+}
